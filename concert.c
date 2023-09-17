8ec5e3ae0d874d9201f3f662581a9d20fff0a1b2
ahmedmohamed991 <ahmmeedhafez22@gmail.com>
2023-09-17 18:27:09 +0300

    ahmed

--- concert.c
+++ concert.c
@@ -1,41 +1,119 @@
 #include "main.h"
-/**
- *convert - function convert decimal to binary
- *@N: first argument
- *
- */
-void convert(unsigned int N)
-{
-	if (N == 0)
-	{
-		printf("o");
-		return;
-	}
-	int binary[33];
-	int i;
 
-	i = 0;
-	while (N > 0)
+/**
+ * print_unsigned - prints an unsigned number
+ * @x: list of arguments
+ * @buffer: Buffer array to handel print
+ * @f: calculate active flags
+ * @width: get width
+ * @p: Precision sp
+ * @s: size specifir
+ * Return: number of chars printed
+ */
+int print_unsigned(va_list x, char buffer[], int width, int f, int s, int p)
+{
+	unsigned long int num = va_arg(x, unsigned long int);
+	int a = size - 2;
+
+	num = convert_size_unsgned(num, s);
+
+	if (num == 0)
+		buffer[a--] = '0';
+	buffer[size - 1] = '\0';
+
+	while (num > 0)
 	{
-		binary[i++] = N % 2;
-		N /= 2;
+		buffer[a--] = (num % 10) + '0';
+		num /= 10;
 	}
-	for (int k = i - 1; i >= 0; i--)
-	{
-		printf("%d", binary[i]);
-	}
+
+	i++;
+	return (write_unsgned(0, a, buffer, f, width, p, s));
 }
+
 /**
- * main - print the binary representation of an unsigned integer
- *
- *
- * Return : 0 alwayes success
+ * print_octal - printed octal number
+ * @x: list of arguments
+ * @buffer: Buffer array to handel print
+ * @f: calculate active flags
+ * @width: get width
+ * @p: precision
+ * @s: size specifir
+ * Return: number of chars printed
  */
-
-int main(void)
+int print_octal(va_list x, char buffer[], int width, int p, int f, int s)
 {
-	unsigned int v = 42;
+	unsigned long int num = va_arg(x, unsigned long int);
+	unsigned long int num2 = num;
+	int a = size - 2;
 
-	printf("binary :%b\n ", value);
-	return (0);
+	UNUSED(width);
+
+	num = convert_size_unsgned(num, s);
+
+	if (num == 0)
+		buffer[a--] = '0';
+
+		buffer[size - 1] = '\0';
+
+		while (num > 0)
+		{
+			buffer[a--] = (num % 8) + '0';
+			num /= 8;
+		}
+
+		if (f & F_HASH && num2 != 0)
+			buffer[a--] = '0';
+
+		a++;
+
+		return (write_unsgned(0, a, buffer, width, size, p, f));
+}
+
+/**
+ *print_hexadecimal - print hexadecimal num
+ * @x: list of argument
+ * @buffer: buffer array to handle print
+ * @f: calculate active flags
+ * @width: get width
+ * @p: precision
+ * @s: size specifier
+ * Return: number of chars printed
+ */
+int print_hexadecimal(va_list x, char buffer[], int width, int s, int p, int f)
+{
+	return (print_hexa(x, "01234556789abcdef", buffer, width, p, s, f, 'x'));
+}
+
+int print_hexa(va_list x, char buffer[], int f, char y[]
+		, int width, int p, int s, char f_ch)
+
+{
+	unsigned long int num = va_arg(x, unsigned long int);
+	unsigned long int num2 = num;
+	int a = size - 2;
+
+	UNUSED(width);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[a--] = '0';
+
+	buffer[size - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[a--] = y[num % 16];
+		num /= 16;
+	}
+	if (f && F_HASH && num2 != 0)
+	{
+		buffer[a--] = y;
+		buffer[a--] = '0';
+	}
+
+	a++;
+
+	return (write_unsgned(0, a, buffer, width, s, p, f));
 }
