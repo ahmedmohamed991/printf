1023d1950f566baf61feb96af618e5369504d0cc
amirasaeed32 <amirasaeed637@gmail.com>
2023-09-17 18:04:19 +0300

    width

---
+++ Width.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _width - compute the width for printing
+ * @f: format string in which to print the arguments
+ * @i: list of arguments to be printed
+ * @l: list of arguments
+ * Return: width
+ */
+int _width(const char *f, int *i, va_list l)
+{
+	int w = 0;
+	int ci;
+
+	while (f[ci] != '\0')
+	{
+		if (__digit(f[ci]))
+		{
+			w *= 10;
+			w += f[ci] - '0';
+			ci++;
+		}
+		else if (f[ci] == '*')
+		{
+			ci++;
+			w = va_arg(l, int);
+			break;
+		}
+		else
+		{
+			break;
+		}
+	}
+	*i = ci - 1;
+	return (w);
+}
